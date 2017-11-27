d58d42a0fdabb38168fdb10bb56503e4a7d94ad3
Claudia Alarcon <claudiia_23_06@hotmail.com>
2017-11-27 15:41:42 -0300

    primer commit

---
+++ prueba.py
@@ -0,0 +1,17 @@
+'''
+Created on 27 nov. 2017
+
+@author: ClaudiaEstefania
+'''
+
+class prueba(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
